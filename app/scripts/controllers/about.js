4d85c36c5d62775d993afa9d3e095e01362a2194
jens brimberg <jens.brimberg@gmail.com>
2015-09-01 06:29:30 +0200

    Yeoman Angular generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name benkeNgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the benkeNgApp
+ */
+angular.module('benkeNgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name benkeNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the benkeNgApp
+ */
+angular.module('benkeNgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
